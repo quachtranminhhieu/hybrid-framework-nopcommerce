8cfacfdf8875ca2ec10274ff2a7b030910070773
Quach Tran Minh Hieu <quachtranminhhieu@gmail.com>
2022-06-22 10:52:37 +0700

    Update file bat

--- RunAllure.bat
+++ RunAllure.bat
@@ -1,5 +1,5 @@
 set ProjectPath=%~dp0
 cd %ProjectPath%
 set p=%PATH%
-java -javaagent:"%ProjectPath%libAllure\aspectjweaver-1.9.8.jar" -classpath "%ProjectPath%bin;%ProjectPath%libAllure\*;%ProjectPath%libAllureReport\*;%ProjectPath%libExtentV5\*;%ProjectPath%libLog4J\*;%ProjectPath%libSelenium\*;%ProjectPath%libReportNG\*;%ProjectPath%libWebDriverManager\*" org.testng.TestNG "%ProjectPath%bin\runNopCommerceTest.xml"
+java -javaagent:"%ProjectPath%libAllure\aspectjweaver-1.9.8.jar" -classpath "%ProjectPath%bin;%ProjectPath%libAllure\*;%ProjectPath%libAllure\*;%ProjectPath%libExtentV5\*;%ProjectPath%libLog4J\*;%ProjectPath%libSelenium\*;%ProjectPath%libReportNG\*;%ProjectPath%libWebDriverManager\*" org.testng.TestNG "%ProjectPath%bin\runNopCommerceTest.xml"
 pause
